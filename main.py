f8003fea6a41c3d23c27db966c0bff1deb3f04d6
simplenikname <3016nikitashatoshin@gmail.com>
2020-01-28 16:39:55 +0300

    nan

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from netaddr import *
+import threading
 
+class Schedule(threading.Thread):
+    def __init__(self, ip_range):
+        super().__init__(self)
+        self.raw_ip_range = ip_range
+
+    def run(self):
+        from_ip, to_ip = self.raw_ip_range.split()
+        range = IPRange(from_ip, to_ip)
