6093b8dcfbb77d56d6e78108d6d00bcb1bbe5f38
simplenikname <3016nikitashatoshin@gmail.com>
2020-01-28 16:56:29 +0300

    non

--- scanner_function.py
+++ scanner_function.py
@@ -2,12 +2,13 @@ from socket import *
 import time 
 
 def scan(ip_table):
-    t_IP = gethostbyname(target)
+    for i in ip_table:
+        t_IP = gethostbyname(target)
 
-    for i in range(50, 500):
-        s = socket(AF_INET, SOCK_STREAM)
+        for i in range(50, 500):
+            s = socket(AF_INET, SOCK_STREAM)
 
-        conn = s.connect((t_IP, i))
-        if (conn == 0):
-            print('Port %d: OPEN' % (i,))
-        s.close()
+            conn = s.connect((t_IP, i))
+            if (conn == 0):
+                print('Port %d: OPEN' % (i,))
+            s.close()
